3c68ad16ee40a94df108d0cd01f8567bef0d37f8
LEO-CAF <leonardo.cafissi.makerhacker@gmail.com>
2022-07-07 13:52:47 +0200

    zero bug - controllo input - solo criptare

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
-chiaro_str=input("Testo in Chiaro: ")
+# tutte le variabili
+
+chiaro_str=""
 chiaro_arr=[]
 scuro_str=""
 scuro_arr=[]
-chiave=int(input("Chiave: "))
-alf=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"]
+key=0
+alf=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+lettera=""
+x=0
 
-chiaro_arr=list(chiaro_str)
+# input > controllo chiaro
+
+while True:
+    chiaro_str=input("Testo in Chiaro: ")
+    if chiaro_str.isalpha():
+        chiaro_arr=list(chiaro_str)
+        break
+    else:
+        print("Inserise solo Caratteri Alfabetici senza Numeri, Spazi e Caratteri Speciali")
+
+# input > controllo key
+
+while True:
+    key=input("Chiave: ")
+    if key.isdigit():
+        key=int(key)
+        break
+    else:
+        print("Inserise solo Numeri Interi Positivi senza Spazi, Punti e Virgole")
+
+# ciclo principale
 
 for i in range(len(chiaro_arr)):
-    lettera=alf[alf.index(chiaro_arr[i])+chiave]
+    x=0
+    while True:
+        try:
+            lettera=alf[alf.index(chiaro_arr[i])+key-x]
+        except:
+            x+=26
+        else:
+            break
+
     scuro_arr.append(lettera)
 
+# output
+
 scuro_str="".join(scuro_arr)
-print(scuro_str)
+print("Testo Criptato:",scuro_str)
